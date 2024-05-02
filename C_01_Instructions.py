31bab79892d1694dac6d87cf30f09004f6e86a08
TrueNewNugget1 <168376355+TrueNewNugget1@users.noreply.github.com>
2024-05-02 14:23:40 +1200

    Fixed yes no error msg, created int checker > 13

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -1,15 +1,57 @@
-print("🎲🎲 Roll it 13 🎲🎲")
+# Checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
 
+        # Checks user response, question
+        # Repeats if users don't enter yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes / no")
+
+
+def instructions():
+    print('''
+
+**** Instructions ****
+
+To begin, decide on a score goal (eg: The first one to get a 
+score of 50 wins).
+
+For each round of the game, you win points by rolling dice.
+The winner of the round is the one who gets 13 (or slightly less).
+
+if you win the round, then your score will increase by the 
+number of points that you earned. If your first roll of two 
+dice is a double (eg: both dice show a three), then your score 
+will be DOUBLE the number of points.
+
+If you lose the round, then you don't get any points.
+
+If you and the computer tie (eg: you both get a score of 11, 
+then you will have 11 points added to your score.)
+
+Your goal is to try to get to the target score before the 
+computer.
+
+Good luck. 
+
+    ''')
+
+# Main routine
+print()
+print("🎲🎲 Roll it 13 🎲🎲")
+print()
 
 # Loop for testing purpose
 
-while True:
-    want_instructions = input("Do you want to read the instructions? ").lower()
+want_instructions = yes_no("Do you want to read the instructions? ")
 
-    # Checks users enter yes (y) or no (n)
-    if want_instructions == "yes" or want_instructions == "y":
-        print("you chose yes")
-    elif want_instructions == "no" or want_instructions == "n":
-        print("you chose no")
-    else:
-        print("You did not choose a valid response")
+# Checks users enter yes (y) or no (n)
+if want_instructions == "yes":
+    instructions()
+
+print("program continues")
